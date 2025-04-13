28647ccc46c06c40ccb68fee5c6828a3f6e5640d
Sanjay <email@example.com>
2025-04-13 21:16:32 +0530

    First

--- Backend/Server.js
+++ Backend/Server.js
@@ -36,7 +36,7 @@ app.get('/products', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
 
